2c62e9da1f130908b68dd7f959d07ab19320bb79
31indiana <68727179+31indiana@users.noreply.github.com>
2021-04-29 11:55:10 +0900

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -2,8 +2,8 @@ import time
 import pyupbit
 import datetime
 
-access = "myaccess"
-secret = "mysecret"
+access = "myaccessmycodemyaccessmycodemyaccessmycode"
+secret = "mysecretmycodemysecretmycodemysecretmycode"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
